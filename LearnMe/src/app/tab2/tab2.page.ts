8257398ea37b58169ce1b08a396c49d4203c67ad
George Vangelatos <40575152+gvangelatos@users.noreply.github.com>
2026-01-09 17:57:40 +0100

    WIP Articles Page

--- LearnMe/src/app/tab2/tab2.page.ts
+++ LearnMe/src/app/tab2/tab2.page.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class Tab2Page {
-
   constructor() {}
-
 }
