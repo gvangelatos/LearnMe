d4c427d72c4ef107f8b86813c8011b464425786c
George Vangelatos <40575152+gvangelatos@users.noreply.github.com>
2026-01-13 21:16:06 +0100

    Introduce choice in articles

--- LearnMe/src/app/tab2/tab2.page.ts
+++ LearnMe/src/app/tab2/tab2.page.ts
@@ -8,6 +8,7 @@ import {
   IonSpinner,
   IonLabel,
   IonIcon,
+  IonText,
 } from '@ionic/angular/standalone';
 import { GsApiService } from '../services/gs-api/gs-api.service';
 import { WordCardModel } from '../tab1/tab1.models';
@@ -30,14 +31,17 @@ import { NgClass } from '@angular/common';
     IonLabel,
     IonIcon,
     NgClass,
+    IonText,
   ],
 })
 export class Tab2Page {
   private readonly gsApiService = inject(GsApiService);
   protected word?: WordCardModel;
-  protected isCorrect = false;
-  protected answered = false;
+  protected isCorrect: boolean = false;
+  protected answered: boolean = false;
   protected isLoading: boolean = false;
+  protected chosenAnswer?: number;
+  protected correctAnswer?: number;
   protected showTranslation: boolean = false;
 
   constructor() {
@@ -46,16 +50,33 @@ export class Tab2Page {
     this.gsApiService.getRandomWordWithArticle().subscribe((x) => {
       if (x.length) {
         this.word = x[0];
+        this.setCorrectWord();
       }
       this.isLoading = false;
     });
   }
 
-  protected answeredClicked(article: string) {
-    console.log(article);
-    if (!this.word) {
+  private setCorrectWord() {
+    if (this.word?.isPlural) {
+      this.correctAnswer = 3;
+    } else {
+      const articles_single = ARTICLES.filter(
+        (x) => !x.toLowerCase().includes('plural'),
+      );
+      const index = articles_single.findIndex((x) =>
+        this.word?.article?.toLowerCase().includes(x.toLowerCase()),
+      );
+      if (index > -1) {
+        this.correctAnswer = index;
+      }
+    }
+  }
+
+  protected answeredClicked(article: string, index: number) {
+    if (!this.word || this.answered) {
       return;
     }
+    this.chosenAnswer = index;
     if (article.toLowerCase().includes('plural') && this.word.isPlural) {
       this.handleAnswer(true);
     } else if (
@@ -74,12 +95,42 @@ export class Tab2Page {
   private handleAnswer(correct: boolean) {
     this.isCorrect = correct;
     this.answered = true;
+    this.showTranslation = true;
+  }
+
+  protected getTextColor() {
+    if (!this.answered) {
+      return 'dark';
+    }
+    if (this.isCorrect) {
+      return 'success';
+    } else {
+      return 'danger';
+    }
   }
 
   private resetQuestion() {
     this.isCorrect = false;
     this.answered = false;
+    this.correctAnswer = undefined;
+    this.chosenAnswer = undefined;
+    this.showTranslation = false;
+    this.word = undefined;
   }
 
   protected readonly ARTICLES = ARTICLES;
+
+  protected getButtonColor(index: number) {
+    if (!this.answered) {
+      return 'dark';
+    }
+    if (index === this.correctAnswer) {
+      return 'success';
+    }
+    if (index === this.chosenAnswer) {
+      return 'danger';
+    }
+
+    return 'dark';
+  }
 }
